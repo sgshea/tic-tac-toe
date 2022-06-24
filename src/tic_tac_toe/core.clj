53b56b6365d62e792ff2b7cf448a1ba1817b63fc
sgshea <sammys512@yahoo.com>
2022-06-24 14:38:39 -0400

    Initial Commit

---
+++ src/tic_tac_toe/core.clj
@@ -0,0 +1,12 @@
+(ns tic-tac-toe.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn hi-person
+  [name]
+  (str "Hi, " name))
+
