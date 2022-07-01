6a6ce024e399fedc00e10689d063316fe7a0c021
sgshea <sammys512@yahoo.com>
2022-07-01 13:58:27 -0400

    Grid now shows different dimensions without error correctly, can be resized and the game restarted.

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -7,43 +7,50 @@
 ;; Board is stored in an atom
 ;; Inital state is with default board of 3x3
 (def *state
-  (atom {:board [1 2 3 4 5 6 7 8 9]
-         :dimensions [3 3]}))
+  (atom {:board [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
+         :dimensions [4 4]
+         :temp-dimensions [3 3]}))
+
+(defn set-dimensions
+  "Updates dimensions and board with new dimensions."
+  [new-dimensions]
+  (swap! *state assoc :dimensions new-dimensions)
+  (swap! *state assoc :board (logic/board-creator new-dimensions)))
 
 (defn grid-cell
   "Gets and returns the display for a single cell.
   To be displayed in the gui.
   Location is position of cell wanted."
-  [board width location]
+  [board location]
   {:fx/type :button
    :grid-pane/hgrow :always
    :grid-pane/vgrow :always
-   :grid-pane/row (inc (first location))
-   :grid-pane/column (inc (last location))
-   :text (nth board (+ (last location) (* (first location) width)))})
+   :grid-pane/row (first location)
+   :grid-pane/column (last location)
+   :text (str (nth (nth board (dec (first location))) (dec (last location))))})
 
 (defn display-board-as-grid
   "Displays the board as a grid."
   [{:keys [board dimensions]}]
   (let [board
-        (map #(if (keyword? %) ; transform keywords (:o or :x) to o and x
+        (partition-all (first dimensions) (map #(if (keyword? %) ; transform keywords (:o or :x) to o and x
                 (str " " (name %))
                 (if (< % 10)
                   (str " " %)
                   %))
-             board)]
+             board))]
     (loop
      [grid '()
-      row 0
-      column 0]
-      (if (< row (last dimensions))
-        (recur (conj grid (grid-cell board (last dimensions) [row column]))
-               (if (< column (dec (first dimensions)))
+      row 1
+      column 1]
+      (if (<= row (last dimensions))
+        (recur (conj grid (grid-cell board [row column]))
+               (if (< column (first dimensions))
                  row
                  (inc row))
-               (if (< column (dec (first dimensions)))
+               (if (< column (first dimensions))
                  (inc column)
-                 0))
+                 1))
         grid))))
 
 ;; Functions for left pane, where information is displayed
@@ -58,7 +65,7 @@
 
 (defn dimension-input
   "Place to input new width and height."
-  [{:keys [dimensions]}]
+  [{:keys [temp-dimensions]}]
   {:fx/type :v-box
    :alignment :center
    :spacing 10
@@ -72,43 +79,40 @@
                            :value-factory {:fx/type :integer-spinner-value-factory
                                            :min min-dimension
                                            :max max-dimension
-                                           :value (first dimensions)}
-                           :on-value-changed #(swap! *state assoc :dimensions [% (last dimensions)])}]}
+                                           :value (first temp-dimensions)}
+                           :on-value-changed #(swap! *state assoc :temp-dimensions [% (last temp-dimensions)])}]}
               {:fx/type :h-box
                :alignment :center
                :spacing 5
                :children [{:fx/type :label
                            :text "Height"}
-
                           {:fx/type :spinner
                            :editable true
                            :value-factory {:fx/type :integer-spinner-value-factory
                                            :min min-dimension
                                            :max max-dimension
-                                           :value (last dimensions)}
-                           :on-value-changed #(swap! *state assoc :dimensions [(first dimensions) %])}]}]})
+                                           :value (last temp-dimensions)}
+                           :on-value-changed #(swap! *state assoc :temp-dimensions [(first temp-dimensions) %])}]}]})
 
 (defn new-game-button
   "Button to start new game."
-  [{:keys [board dimensions]}]
+  [{:keys [temp-dimensions]}]
   {:fx/type :button
    :text "New Game"
-   :on-action (swap! *state assoc :dimensions dimensions
-                     :board (logic/board-creator dimensions))})
+   :on-action (fn [_] (set-dimensions temp-dimensions))})
 
 (defn display-information
   "A place to display information such as who's turn it is."
-  [{:keys [board dimensions information]}]
-  {:fx/type :v-box
+  [{:keys [temp-dimensions information]}]
+  {:fx/type   :v-box
    :alignment :center
-   :spacing 20
-   :children [{:fx/type :label
-               :text (str information)}
-              {:fx/type dimension-input
-               :dimensions dimensions}
-              {:fx/type new-game-button
-               :board board
-               :dimensions dimensions}]})
+   :spacing   20
+   :children  [{:fx/type :label
+                :text    (str information)}
+               {:fx/type         dimension-input
+                :temp-dimensions temp-dimensions}
+               {:fx/type         new-game-button
+                :temp-dimensions temp-dimensions}]})
 
 ;; Grid for tic-tac-toe board
 (defn grid-pane
@@ -120,7 +124,7 @@
 
 ;; Splits scene into grid-pane and a place for other text
 (defn split-pane
-  [{:keys [board dimensions]}]
+  [{:keys [board dimensions temp-dimensions]}]
   {:fx/type :split-pane
    :divider-positions [0.5]
    :items [{:fx/type :v-box
@@ -130,15 +134,14 @@
                         :text "Tic-Tac-Toe"}
                        {:fx/type display-information
                         :padding 50
-                        :information (str "Current dimensions " dimensions)
-                        :board board
-                        :dimensions dimensions}]}
+                        :information (str "Current Dimensions: " (str dimensions))
+                        :temp-dimensions temp-dimensions}]}
            {:fx/type grid-pane
             :board board
             :dimensions dimensions}]})
 
 (defn root
-  [{:keys [board dimensions]}]
+  [{:keys [board dimensions temp-dimensions]}]
   {:fx/type :stage
    :showing true
    :title "Tic-Tac-toe"
@@ -147,12 +150,8 @@
                   :pref-width 960
                   :pref-height 540
                   :board board
-                  :dimensions dimensions}}})
-
-(defn set-board
-  "Updates board."
-  [board]
-  (swap! *state assoc :board board))
+                  :dimensions dimensions
+                  :temp-dimensions temp-dimensions}}})
 
 (def renderer
   (fx/create-renderer
