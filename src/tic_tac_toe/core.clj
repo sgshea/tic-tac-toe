43d129215d6f9cec7efaa12adfcd4c9641289624
sgshea <sammys512@yahoo.com>
2022-06-30 20:53:13 -0400

    View elements in place, backend functionality todo.

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -4,12 +4,18 @@
             [tic-tac-toe.logic :as logic])
   (:import [javafx.application Platform]))
 
+;; Board is stored in an atom
+;; Inital state is with default board of 3x3
+(def *state
+  (atom {:board [1 2 3 4 5 6 7 8 9]
+         :dimensions [3 3]}))
+
 (defn grid-cell
   "Gets and returns the display for a single cell.
   To be displayed in the gui.
   Location is position of cell wanted."
   [board width location]
-  {:fx/type :label
+  {:fx/type :button
    :grid-pane/hgrow :always
    :grid-pane/vgrow :always
    :grid-pane/row (inc (first location))
@@ -40,14 +46,69 @@
                  0))
         grid))))
 
+;; Functions for left pane, where information is displayed
+;; 1. Who's turn it is at the top
+;; 2. Way to change board size
+;; 3. Button to start new game
+
+(def min-dimension
+  3)
+(def max-dimension
+  20)
+
+(defn dimension-input
+  "Place to input new width and height."
+  [{:keys [dimensions]}]
+  {:fx/type :v-box
+   :alignment :center
+   :spacing 10
+   :children [{:fx/type :h-box
+               :alignment :center
+               :spacing 5
+               :children [{:fx/type :label
+                           :text "Width"}
+                          {:fx/type :spinner
+                           :editable true
+                           :value-factory {:fx/type :integer-spinner-value-factory
+                                           :min min-dimension
+                                           :max max-dimension
+                                           :value (first dimensions)}
+                           :on-value-changed #(swap! *state assoc :dimensions [% (last dimensions)])}]}
+              {:fx/type :h-box
+               :alignment :center
+               :spacing 5
+               :children [{:fx/type :label
+                           :text "Height"}
+
+                          {:fx/type :spinner
+                           :editable true
+                           :value-factory {:fx/type :integer-spinner-value-factory
+                                           :min min-dimension
+                                           :max max-dimension
+                                           :value (last dimensions)}
+                           :on-value-changed #(swap! *state assoc :dimensions [(first dimensions) %])}]}]})
+
+(defn new-game-button
+  "Button to start new game."
+  [{:keys [board dimensions]}]
+  {:fx/type :button
+   :text "New Game"
+   :on-action (swap! *state assoc :dimensions dimensions
+                     :board (logic/board-creator dimensions))})
+
 (defn display-information
   "A place to display information such as who's turn it is."
-  [{:keys [information]}]
-  {:fx/type :h-box
+  [{:keys [board dimensions information]}]
+  {:fx/type :v-box
    :alignment :center
    :spacing 20
    :children [{:fx/type :label
-               :text (str information)}]})
+               :text (str information)}
+              {:fx/type dimension-input
+               :dimensions dimensions}
+              {:fx/type new-game-button
+               :board board
+               :dimensions dimensions}]})
 
 ;; Grid for tic-tac-toe board
 (defn grid-pane
@@ -69,7 +130,9 @@
                         :text "Tic-Tac-Toe"}
                        {:fx/type display-information
                         :padding 50
-                        :information "test information!"}]}
+                        :information (str "Current dimensions " dimensions)
+                        :board board
+                        :dimensions dimensions}]}
            {:fx/type grid-pane
             :board board
             :dimensions dimensions}]})
@@ -86,22 +149,10 @@
                   :board board
                   :dimensions dimensions}}})
 
-;; Board is stored in an atom
-;; Inital state is with default board of 3x3
-(def *state
-  (atom {:board [1 2 3 4 5 6 7 8 9]
-         :dimensions [3 3]}))
-
 (defn set-board
   "Updates board."
   [board]
   (swap! *state assoc :board board))
-
-(defn set-dimensions
-  "Updates dimensions and the board with given dimensions."
-  [dimensions]
-  (swap! *state assoc :dimensions dimensions
-         :board (logic/board-creator dimensions)))
 
 (def renderer
   (fx/create-renderer
