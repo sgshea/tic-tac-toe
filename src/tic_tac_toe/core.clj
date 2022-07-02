de042967908b599388b79426c84828eb388dc85e
sgshea <sammys512@yahoo.com>
2022-07-02 17:54:14 -0400

    Prevent moves after game is won, colors for players.

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -5,13 +5,20 @@
             [clojure.string :as str])
   (:import [javafx.application Platform]))
 
-;; Board is stored in an atom
-;; Inital state is with default board of 3x3
+; Global state
+; board defaults to 3x3
+; first player is :o
 (def *state
   (atom {:board [1 2 3 4 5 6 7 8 9]
          :dimensions [3 3]
          :temp-dimensions [3 3]
-         :player-turns logic/player-turns}))
+         :player-turns logic/player-turns
+         :winner [false nil]}))
+
+(def min-dimension
+  3)
+(def max-dimension
+  20)
 
 (defn set-dimensions
   "Updates dimensions and board with new dimensions."
@@ -19,14 +26,32 @@
   (swap! *state assoc :dimensions new-dimensions)
   (swap! *state assoc :board (logic/board-creator new-dimensions)))
 
+(defn set-winner
+  "Updates winner."
+  [bool player]
+  (swap! *state assoc :winner [bool player]))
+
 (defn board-placement
   "Places next player onto board with given location."
   [board cell-data]
-  (if (keyword? cell-data)
+  (if (or (first (@*state :winner)) (keyword? cell-data))
     nil
     (do
       (swap! *state assoc :board (assoc board (dec cell-data) (first (@*state :player-turns))))
       (swap! *state assoc :player-turns (rest (@*state :player-turns))))))
+
+(defn cell-background
+  "Controls cell color depending on player, if they won, etc."
+  [cell-data]
+  (if (= cell-data (last (@*state :winner)))
+    (if (= cell-data :o)
+      {:-fx-background-color :forestgreen}
+      {:-fx-background-color :tomato})
+    (if (keyword? cell-data)
+      (if (= cell-data :o)
+        {:-fx-background-color :darkseagreen}
+        {:-fx-background-color :lightcoral})
+      {:-fx-background-color :lightgray})))
 
 (defn grid-cell
   "Gets and returns the display for a single cell.
@@ -36,8 +61,11 @@
   (let [cell-data ; what is located in the cell
         (nth (nth board (dec (first location))) (dec (last location)))]
     {:fx/type          :button
-     :grid-pane/hgrow  :always
-     :grid-pane/vgrow  :always
+     :style (cell-background cell-data)
+     :grid-pane/hgrow  :sometimes
+     :grid-pane/vgrow  :sometimes
+     :max-width Double/MAX_VALUE
+     :max-height Double/MAX_VALUE
      :grid-pane/row    (first location)
      :grid-pane/column (last location)
      :on-action        (fn [_] (board-placement (vec (flatten board)) cell-data))
@@ -64,11 +92,6 @@
                  1))
         grid))))
 
-;; Functions for left pane, where information is displayed
-;; 1. Who's turn it is at the top
-;; 2. Way to change board size
-;; 3. Button to start new game
-
 (defn information
   "Checks for winners, if board is full, or else displays current turn. Also displays current dimensions."
   [{:keys [board dimensions player]}]
@@ -78,15 +101,12 @@
    :children [{:fx/type :label
                :text (let [winner (logic/winner? board dimensions)]
                        (cond
-                         winner (str "Player " (name winner) " wins!")
+                         winner (do
+                                  (set-winner true winner)
+                                  (str "Player " (name winner) " wins!"))
                          (logic/full-board? board) (str "The game is a draw.")
                          :else
                          (str (name player) "'s turn.")))}]})
-
-(def min-dimension
-  3)
-(def max-dimension
-  20)
 
 (defn dimension-input
   "Place to input new width and height."
@@ -124,7 +144,9 @@
   [{:keys [temp-dimensions]}]
   {:fx/type :button
    :text "New Game"
-   :on-action (fn [_] (set-dimensions temp-dimensions))})
+   :on-action (fn [_] (do
+                        (set-dimensions temp-dimensions)
+                        (set-winner false nil)))})
 
 (defn display-information
   "Left of split pane with controls and information display."
@@ -141,19 +163,19 @@
                {:fx/type         new-game-button
                 :temp-dimensions temp-dimensions}]})
 
-;; Grid for tic-tac-toe board
 (defn grid-pane
+  "Contains the grid for the tic-tac-toe board."
   [{:keys [board dimensions]}]
   {:fx/type :grid-pane
    :children
    (display-board-as-grid {:board board
                            :dimensions dimensions})})
 
-;; Splits scene into grid-pane and a place for other text
 (defn split-pane
+  "Splits scene into grid-pane, and left-hand section for everything else."
   [{:keys [board dimensions temp-dimensions]}]
   {:fx/type :split-pane
-   :divider-positions [0.5]
+   :divider-positions [0.2]
    :items [{:fx/type :v-box
             :split-pane/resizable-with-parent false
             :children [{:fx/type :label
@@ -165,6 +187,7 @@
                         :dimensions dimensions
                         :temp-dimensions temp-dimensions}]}
            {:fx/type grid-pane
+            :split-pane/resizable-with-parent false
             :board board
             :dimensions dimensions}]})
 
@@ -172,6 +195,7 @@
   [{:keys [board dimensions temp-dimensions]}]
   {:fx/type :stage
    :showing true
+   :maximized true
    :title "Tic-Tac-toe"
    :scene {:fx/type :scene
            :root {:fx/type split-pane
@@ -189,7 +213,3 @@
   [& args]
   (Platform/setImplicitExit true)
   (fx/mount-renderer *state renderer))
-
-;; TODO
-;; Probably need to reimplement most of the cli functionality to use javafx text boxes for input etc.
-;; store the board in the atom and update on clicks? then display next turn
